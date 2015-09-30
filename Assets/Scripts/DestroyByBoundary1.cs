03023cee2703b727bebbf5359c4ac3778b43c15b
Siddharth Gupta <sidystan@gmail.com>
2015-10-01 00:37:57 +0530

    Done and Dusted

---
+++ Assets/Scripts/DestroyByBoundary1.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary1 : MonoBehaviour {
+
+	void OnTriggerExit(Collider other){
+		Destroy (other.gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public int waveWait;
+
+	void Start(){
+		StartCoroutine(spawnWaves ());
+	}
+
+	IEnumerator spawnWaves(){
+		yield return new WaitForSeconds(startWait);
+		while(true){
+			for (int i = 0; i < hazardCount; i++) {
+			
+				Vector3 spawnposition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnrotation = Quaternion.identity;
+				Instantiate (hazard, spawnposition, spawnrotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
